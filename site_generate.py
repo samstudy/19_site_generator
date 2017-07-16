bdbe9fbcd5dd4bfc6c1c0dfa1056aee6c2554877
Samat Karymshakov <bskordo@gmail.com>
2017-07-16 12:40:27 +0600

    Separated render template function from generate functions

--- site_generate.py
+++ site_generate.py
@@ -1,4 +1,4 @@
-import jinja2
+from jinja2 import Template
 import os
 import json
 import argparse
@@ -40,33 +40,35 @@ def generate_pages(data, filepath, template):
     with open(filepath, 'w', encoding='utf-8') as f:
         f.write(template.render(info=data))
 
-
-def generate_articles_html(structure, template='templates/base.html'):
+def render_template(template):
     with open(template, encoding = 'utf-8') as html_page:
-        template = jinja2.Template(html_page.read())
-        for item in structure['articles']:
-            text = convert_md_to_html(join('articles', item['source']))
-            context = {
-            'title': item['title'],
-            'topic': item['topic'],
-            'text' : text
-            }
-            path_to_save = join(DIR_TO_SAVE, item['source'])
-            final_path = sub(r'&.+?.md$|.md$', '.html', path_to_save)
-            create_dir_for_file(final_path)
-            generate_pages(context, final_path, template) 
+        template = Template(html_page.read())
+        return template
+
+def generate_articles_html(structure, template):
+    for item in structure['articles']:
+        text = convert_md_to_html(join('articles', item['source']))
+        context = {
+        'title': item['title'],
+        'topic': item['topic'],
+        'text' : text
+        }
+        path_to_save = join(DIR_TO_SAVE, item['source'])
+        final_path = sub(r'&.+?.md$|.md$', '.html', path_to_save)
+        create_dir_for_file(final_path)
+        generate_pages(context, final_path, template) 
 
 
-def generate_index_html(structure, template='templates/index.html'):
-    with open(template, encoding = 'utf-8') as html_page:
-        template = jinja2.Template(html_page.read())
-        for item in structure['articles']:
-            path_to_save = join(DIR_TO_SAVE, item['source'])
-            item['source'] = sub(r'&.+?.md$|.md$', '.html', path_to_save)
-            generate_pages(structure, DIR_TO, template) 
+def generate_index_html(structure, template):
+    for item in structure['articles']:
+        path_to_save = join(DIR_TO_SAVE, item['source'])
+        item['source'] = sub(r'&.+?.md$|.md$', '.html', path_to_save)
+        generate_pages(structure, DIR_TO, template) 
 
 if __name__ == '__main__':
     get_structure_location = get_args()
     structure = load_structure(get_structure_location)
-    generate_articles_html(structure)
-    generate_index_html(structure)
+    articles_template = render_template('templates/base.html')
+    generate_articles_html(structure,articles_template)
+    index_template = render_template('templates/index.html')
+    generate_index_html(structure,index_template)
